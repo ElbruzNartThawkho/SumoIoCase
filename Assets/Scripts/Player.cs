b5ec2d990f221786a228004f2678fe6c979f0233
Elbruz Nart Şuruh <57918029+ElbruzNartThawkho@users.noreply.github.com>
2023-07-09 04:01:48 +0300

    23.7.9
    -bu son olsun bu son

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -225,6 +225,7 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             PlayerListUpdate();
+            locationTraveled = GetNearestPlayerPosition();
         }
     }
 }
